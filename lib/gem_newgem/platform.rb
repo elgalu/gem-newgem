730ed43fdf91a98635be24a10fb1ce8ae6ee086d
Leo Gallucci <elgalu3@gmail.com>
2013-02-28 18:59:35 -0300

    Updated gem summary

--- lib/gem_newgem/platform.rb
+++ lib/gem_newgem/platform.rb
@@ -1,4 +1,4 @@
 module GemNewgem
   VERSION = "0.1.0"
-  SUMMARY = %q{Generate new gem skeleton based on a default or custom template.}
+  SUMMARY = %q{RubyGems plugin to generate a new gem skeleton based customizable templates.}
 end
